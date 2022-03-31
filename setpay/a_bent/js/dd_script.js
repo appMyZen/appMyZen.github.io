8c017ab3270e5bded5355f8237e5703f913bd48a
appMyZen <95835612+appMyZen@users.noreply.github.com>
2022-03-31 19:54:29 +0500

    Update dd_script.js

--- setpay/a_bent/js/dd_script.js
+++ setpay/a_bent/js/dd_script.js
@@ -2,14 +2,17 @@
  * Created by Dmitry-xeon-v3 on 031 31.03.22.
  */
 
-alert('платеж да 2');
+alert('платеж да 3');
 //history.go(-8);
 
 //window.close();
 
 function closeWindow(){
-	if (confirm('Перейти в приложение 2')) {
+	if (confirm('Перейти в приложение 3')) {
+		alert('переход ');
 		myapp.views.main.router.navigate('/input_name/');
+		alert('проб выйти ');
+		navigator.app.exitApp();
 	}
 }
 
