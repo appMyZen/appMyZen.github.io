39058781e4744ef70a27c4bb73c0c682779206c8
appMyZen <95835612+appMyZen@users.noreply.github.com>
2022-04-03 20:06:21 +0500

    Add files via upload

---
+++ getpay/a_bent/js/dd_script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Dmitry-xeon-v3 on 031 31.03.22.
+ */
+
+alert('платеж прошел_01');
+
+
+
+//history.go(-8);
+
+// alert('ddd_good');
+//
+// setTimeout(function() {
+//
+//   alert('ddd_good_555');
+//
+//   //window.location.href = 'index.html';
+//
+//   //history.back(2);
+//
+//   history.go(-8);
+//
+// }, 5000);
